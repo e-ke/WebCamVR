ffa9990641f9cd30ca687e1af73224c0ad6325cc
e-ke <89178778+e-ke@users.noreply.github.com>
2023-12-04 14:30:00 +0900

    キー入力のログcsv出力実装

--- Assets/MyAssets/MovMoQ/Scripts/MoQInput.cs
+++ Assets/MyAssets/MovMoQ/Scripts/MoQInput.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,7 +14,21 @@ public class MoQInput : MonoBehaviour
     [SerializeField] private HMDLogger _HMDLogger;
     [SerializeField] private VideoController _VideoController;
 
-    
+    private string logFilePath;
+
+    void Start()
+    {
+        // ログファイルのパスを設定
+        string logDirectory = Application.dataPath + "/MyFileIO/logs/";
+        if (!Directory.Exists(logDirectory))
+        {
+            Directory.CreateDirectory(logDirectory);
+        }
+        logFilePath = logDirectory + "Input_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".csv";
+        // CSVヘッダーを追加
+        File.AppendAllText(logFilePath, "Time,KeyPressed\n");
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -23,48 +38,77 @@ public class MoQInput : MonoBehaviour
         if (current == null) return;
 
         // 上下左右キー
-        if (current.upArrowKey.wasPressedThisFrame) _Lights_T.Run();
-        if (current.downArrowKey.wasPressedThisFrame) _Lights_B.Run();
-        if (current.leftArrowKey.wasPressedThisFrame) _Lights_L.Run();
-        if (current.rightArrowKey.wasPressedThisFrame) _Lights_R.Run();
-        
+        if (current.upArrowKey.wasPressedThisFrame)
+        {
+            LogKey("Up", "moQ");
+            _Lights_T.Run();
+        }
+        if (current.downArrowKey.wasPressedThisFrame){
+            LogKey("Down", "moQ");
+             _Lights_B.Run();
+        }
+        if (current.leftArrowKey.wasPressedThisFrame){
+            LogKey("Left", "moQ");
+             _Lights_L.Run();
+        }
+        if (current.rightArrowKey.wasPressedThisFrame){
+            LogKey("Right", "moQ");
+             _Lights_R.Run();
+        }
+
+
         // スペースキー
         if (current.spaceKey.wasPressedThisFrame)
         {
+            LogKey("Space", "start");
             _HMDLogger.StartHMDLog();
             _VideoController.PlayVideo();
-
         }
-        
+
 
         // テンキーの数字キー
         if (current.numpad0Key.wasPressedThisFrame)
         {
+            LogKey("Numpad0", "key");
             Debug.Log("テンキーの0が押された");
         }
         if (current.numpad1Key.wasPressedThisFrame)
         {
+            LogKey("Numpad1", "key");
             Debug.Log("テンキーの1が押された");
         }
         if (current.numpad2Key.wasPressedThisFrame)
         {
+            LogKey("Numpad2", "key");
             Debug.Log("テンキーの2が押された");
         }
         if (current.numpad3Key.wasPressedThisFrame)
         {
+            LogKey("Numpad3", "key");
             Debug.Log("テンキーの3が押された");
         }
         if (current.numpad4Key.wasPressedThisFrame)
         {
+            LogKey("Numpad4", "key");
             Debug.Log("テンキーの4が押された");
         }
         if (current.numpad5Key.wasPressedThisFrame)
         {
+            LogKey("Numpad5", "key");
             Debug.Log("テンキーの5が押された");
         }
         if (current.numpad6Key.wasPressedThisFrame)
         {
+            LogKey("Numpad6", "key");
             Debug.Log("テンキーの6が押された");
         }
+    }
+
+    private void LogKey(string keyName, string eventName)
+    {
+        // データをCSV形式でフォーマット
+        string logData = $"{Time.time},{keyName},{eventName}\n";
+        // CSVファイルにデータを追記
+        File.AppendAllText(logFilePath, logData);
     }
 }
